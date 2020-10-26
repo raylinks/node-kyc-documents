f5f9ce360263b08af2c70ead35db631022b1051e
rilwan <olawuyirilwan98@gmail.com>
2020-10-26 10:04:19 +0100

    setting up verification for sendotp

---
+++ src/controllers/VerificationController.js
@@ -0,0 +1,16 @@
+const  {} = require('../models')
+//const uuidv1 = require('uuid/v1');
+const  multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+     async sendOtp(req,res){
+         try{
+            
+         }catch(err){
+             console.log(err);
+         }
+    },
+}
+
--- src/routes.js
+++ src/routes.js
@@ -5,18 +5,22 @@ const  RegisterController = require('./controllers/RegisterController');
 const  LoginController = require('./controllers/LoginController');
 
 module.exports = (app) => {
+// Authentication routes
+   app.post('/api/register/create', 
+       RegisterController.register)
+
+   app.post('/api/auth/login', 
+       LoginController.login)
+
+//DocumentType Routes
     app.get('/api/all', 
        DocumentTypeController.index)
 
     app.get('/api/show/:slug', 
        DocumentTypeController.show)
 
+// Document Routes
    app.get('/api/document/index', 
        DocumentController.index)
 
-   app.post('/api/register/create', 
-       RegisterController.register)
-
-   app.post('/api/auth/login', 
-       LoginController.login)
 }
